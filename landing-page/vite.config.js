0a07947998ff0a88f95fb00f84b940983d0e8a76
aniadev <41569332+aniadev@users.noreply.github.com>
2026-06-05 20:00:42 +0700

    feat: enhance Vite configuration to include sitemap generation on build completion
    - Updated vite.config.d.ts to support additional UserConfig types.
    - Modified vite.config.js and vite.config.ts to add ssgOptions for generating a sitemap.xml file.
    - Implemented a function to create a sitemap with the current date and write it to the dist directory after the build process.

--- landing-page/vite.config.js
+++ landing-page/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import fs from 'fs';
 export default defineConfig({
     plugins: [vue()],
     resolve: {
@@ -8,4 +9,15 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
+    // @ts-ignore
+    ssgOptions: {
+        script: 'async',
+        formatting: 'minify',
+        onFinished: function () {
+            var hostname = 'https://android-stream-desk.vercel.app';
+            var sitemap = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n  <url>\n    <loc>".concat(hostname, "/</loc>\n    <lastmod>").concat(new Date().toISOString().split('T')[0], "</lastmod>\n    <changefreq>monthly</changefreq>\n    <priority>1.0</priority>\n  </url>\n</urlset>");
+            fs.writeFileSync(path.resolve(__dirname, 'dist/sitemap.xml'), sitemap);
+            console.log('Sitemap generated successfully!');
+        }
+    }
 });
--- landing-page/vite.config.ts
+++ landing-page/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [vue()],
@@ -9,4 +10,23 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  // @ts-ignore
+  ssgOptions: {
+    script: 'async',
+    formatting: 'minify',
+    onFinished() {
+      const hostname = 'https://android-stream-desk.vercel.app'
+      const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${hostname}/</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`
+      fs.writeFileSync(path.resolve(__dirname, 'dist/sitemap.xml'), sitemap)
+      console.log('Sitemap generated successfully!')
+    }
+  }
 })
